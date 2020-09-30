12e9bab2ff3d83918bc33054e680ed84e7e487c8
Adrianfer <adrianfermartinez6@gmail.com>
2020-09-30 06:38:17 -0400

    Incorporacion del meta viewport y ajustes de tamaños

---
+++ js/carrucel.js
@@ -0,0 +1,10 @@
+new Splide('.splide-hamburguesa', {
+      perPage: 1,
+      gap: '2rem',
+      arrows:false,
+      autoplay:true,
+      resetProgress: true,
+      640: {
+			perPage: 2,
+		}
+    }).mount();
