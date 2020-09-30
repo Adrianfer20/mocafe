3543a5a00e26e22607e8d7d150ad1faaac8c5bff
Adrianfer <adrianfermartinez6@gmail.com>
2020-09-30 08:07:40 -0400

    agg pizza

--- js/carrucel.js
+++ js/carrucel.js
@@ -1,10 +1,35 @@
 new Splide('#splideHamburguesa', {
-      perPage: 1,
+      perPage: 3,
       gap: '2rem',
       arrows:false,
       autoplay:true,
       resetProgress: true,
-      640: {
+      breakpoints: {
+		'768': {
 			perPage: 2,
-		}
+			gap    : '1rem',
+		},
+        '425': {
+			perPage: 1,
+			gap    : '1rem',
+        }
+    }
     }).mount();
+
+    new Splide('#splidePizza', {
+        perPage: 3,
+        gap: '2rem',
+        arrows:false,
+        autoplay:true,
+        resetProgress: true,
+        breakpoints: {
+          '768': {
+              perPage: 2,
+              gap    : '1rem',
+          },
+          '425': {
+              perPage: 1,
+              gap    : '1rem',
+          }
+      }
+      }).mount();
