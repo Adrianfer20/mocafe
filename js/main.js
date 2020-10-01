cac86daf9b5b8d418511f8781d0fa03030ec8172
Adrianfer <adrianfermartinez6@gmail.com>
2020-10-01 17:26:40 -0400

    funcion btn navbar activo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 (function(){
     const $id = (selector)=>{return document.getElementById(selector)}
 
+
     const $navbar = $id("navbar");
     const $sectionH = $id("section-h");
     const $sectionP = $id('section-p');
@@ -8,13 +9,28 @@
 
     $navbar.addEventListener('click', (e)=>{
         if(e.target.id === 'btn-burguer' || e.target.parentElement.id === 'btn-burguer'){
-            window.scroll(0, $sectionH.offsetTop - 20)
+            window.scroll(0, $sectionH.offsetTop - 20);
+            document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
+            if(!e.target.matches('button')){
+                return e.target.parentElement.classList.add('bg-orange-600');
+            }
+            return e.target.classList.add('bg-orange');
         }
         if(e.target.id === 'btn-pizza' || e.target.parentElement.id === 'btn-pizza'){
-            window.scroll(0, $sectionP.offsetTop - 20)
+            window.scroll(0, $sectionP.offsetTop - 20);
+            document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
+            if(!e.target.matches('button')){
+                return e.target.parentElement.classList.add('bg-orange-600');
+            }
+            return e.target.classList.add('bg-orange');
         }
         if(e.target.id === 'btn-bebida' || e.target.parentElement.id === 'btn-bebida'){
-            window.scroll(0, $sectionB.offsetTop - 20)
+            window.scroll(0, $sectionB.offsetTop - 20);
+            document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
+            if(!e.target.matches('button')){
+                return e.target.parentElement.classList.add('bg-orange-600');
+            }
+            return e.target.classList.add('bg-orange');
         }
     })
 })();
