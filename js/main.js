e48c7eb8672bbb19faaf1b4bbb3bea38d78ee51e
Adrianfer <adrianfermartinez6@gmail.com>
2020-10-02 06:45:01 -0400

    arreglo de button navbar activo, habia olvidado poner la clase bg-orange-600

--- js/main.js
+++ js/main.js
@@ -9,33 +9,36 @@
 
     $navbar.addEventListener('click', (e)=>{
         if(e.target.id === 'btn-burguer' || e.target.parentElement.id === 'btn-burguer'){
+            console.log(e.target);
             window.scroll(0, $sectionH.offsetTop - 20);
             document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
-            if(e.target.parentElement.matches('button')){
+            if(e.target.matches('img')){
                 return e.target.parentElement.classList.add('bg-orange-600');
             }
-            else{
-                return e.target.classList.add('bg-orange');
+            else if (e.target.matches('button')){
+                return e.target.classList.add('bg-orange-600');
             }
         }
         if(e.target.id === 'btn-pizza' || e.target.parentElement.id === 'btn-pizza'){
+            console.log(e.target);
             window.scroll(0, $sectionP.offsetTop - 20);
             document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
-            if(e.target.parentElement.matches('button')){
+            if(e.target.matches('img')){
                 return e.target.parentElement.classList.add('bg-orange-600');
             }
-            else{
-                return e.target.classList.add('bg-orange');
+            else if (e.target.matches('button')){
+                return e.target.classList.add('bg-orange-600');
             }
         }
         if(e.target.id === 'btn-bebida' || e.target.parentElement.id === 'btn-bebida'){
+            console.log(e.target);
             window.scroll(0, $sectionB.offsetTop - 20);
             document.querySelector("button.bg-orange-600").classList.remove('bg-orange-600');
-            if(e.target.parentElement.matches('button')){
+            if(e.target.matches('img')){
                 return e.target.parentElement.classList.add('bg-orange-600');
             }
-            else{
-                return e.target.classList.add('bg-orange');
+            else if (e.target.matches('button')){
+                return e.target.classList.add('bg-orange-600');
             }
         }
     })
